3ee8015cb09a550546c7ef82e5aee4f4796d7726
Quentin Gibson <quentingibson94@gmail.com>
2022-06-17 14:40:10 -0400

    uncomment all routes

--- back/app.js
+++ back/app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const postRoutes = require("./routes/post");
 
 require("dotenv").config();
 
@@ -13,9 +14,9 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
-// app.use("/api/auth", authRoutes);
-// app.use("/api/user", userRoutes);
-// app.use("/api/post", postRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 if (process.env.NODE_ENV === "development") {
   app.use(errorhandler());
 }
