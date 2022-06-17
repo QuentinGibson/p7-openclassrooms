2853b7e816906f57215299f0092bf0c99541f01b
Quentin Gibson <quentingibson94@gmail.com>
2022-06-17 14:40:29 -0400

    remove multer from users

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const multer = require("../middleware/multer-config");
 const auth = require("../middleware/auth");
 const { deleteUser, updateUser, getUser } = require("../controllers/user");
 
 const router = express.Router();
 
 router.get("/:id", auth, getUser);
-router.put("/:id", auth, multer, updateUser);
+router.put("/:id", auth, updateUser);
 router.delete("/:id", auth, deleteUser);
 
 module.exports = router;
