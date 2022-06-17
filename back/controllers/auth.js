a87ca2be9d2dda9e5e903f79804c8492a2b77873
Quentin Gibson <quentingibson94@gmail.com>
2022-06-16 22:38:14 -0400

    add comment for future auth logic

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -2,16 +2,26 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 module.exports = {
-  signUp: async (formData) => {
-    const { firstName, lastName, email, pass } = formData;
+  signUp: async (req, res, next) => {
+    const { firstName, lastName, email, pass } = req.body;
     const password = await bcrypt.hash(pass, 13);
+    //Store password into the database
   },
-  signIn: async (formData) => {
-    const { email, pass } = formData;
+  signIn: async (req, res, next) => {
+    const { email, pass } = req.body;
+    //Find user by email using sequalize
+    //Add storedHash
 
-    bcrypt.compare(pass, hash).then((match) => {
+    bcrypt.compare(pass, storedHash).then((match) => {
       if (match) {
         const token = jwt.sign({ user_id }, process.env.TOKEN_SECRET);
+        res.status(200).json({
+          token,
+        });
+      } else {
+        res.status(400).json({
+          message: "Login Invalid",
+        });
       }
     });
   },
